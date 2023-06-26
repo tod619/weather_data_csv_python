8cdf75a03754e51a7a7e7f7400d14ffe4bb26498
tod619 <trevorodwyer619@hotmail.com>
2023-06-26 20:10:49 +0100

    extract temps + store in list as ints in main.py file

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ import csv
 
 with open("weather_data.csv") as data_file:
     data = csv.reader(data_file)
+    temperatures = []
     for row in data:
-        print(row)
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
+
+print(temperatures)
